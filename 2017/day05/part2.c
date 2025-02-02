77f0fa9ff3b91de9334a53f5003884db1ab9804b
Robin Gertenbach <RobinGertenbach@gmail.com>
2025-02-01 20:37:43 -0800

    Day 5 2017

---
+++ 2017/day05/part2.c
@@ -0,0 +1,42 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXLEN 2000
+
+int
+main(int const argc, char const * const * const argv)
+{
+    if (argc < 2) {
+        fprintf(stderr, "Need to supply filename\n");
+        exit(1);
+    }
+    FILE * f = fopen(argv[1], "r");
+    if (!f) {
+        fprintf(stderr, "Could not open %s\n", argv[1]);
+        exit(1);
+    }
+    char * s = malloc(MAXLEN);
+    int jumps[MAXLEN];
+    size_t n = 0;
+
+    while (fgets(s, MAXLEN, f)) {
+        s[strlen(s) - 1] = '\0'; // Trim newline.
+        sscanf(s, "%d", jumps + (n++));
+    }
+    fclose(f);
+    size_t p = 0;
+    size_t steps = 0;
+    while (p < n) {
+        ptrdiff_t jump = jumps[p];
+        if (jump >= 3) {
+            jumps[p]--;
+        } else {
+            jumps[p]++;
+        }
+        p += jump;
+        steps++;
+    }
+    printf("%zu\n", steps);
+}
